dfc697cf60388a66bdc9718b7acf82c87905ee3b
Danil Annenkov <annenkov@ib-soft.ru>
2014-03-24 14:21:48 +0900

    Initial commit

---
+++ src/main/scala/sll/Parser.scala
@@ -0,0 +1,57 @@
+package sll.parsing
+
+import scala.util.parsing.combinator.syntactical.StandardTokenParsers
+import scala.util.parsing.combinator.ImplicitConversions
+import scala.util.parsing.combinator.token.StdTokens
+import sll.syntax._
+
+object SllParser extends StandardTokenParsers with ImplicitConversions {
+  lexical.delimiters += (",", "=", "(", ")")
+  lexical.reserved += ("program")
+  
+  def start: Parser[List[Definition]] = rep(definition)
+  
+  def definition = fDef | gDef
+  
+  def fDef: Parser[Definition] =
+    funcName ~ ("(" ~> repsep(variable, ",") <~ ")") ~ ("=" ~> expr) ^^ FDef
+  
+  def gDef: Parser[Definition] =
+    funcName ~ ("(" ~> pat) ~ (gDefVars <~ ")") ~ ("=" ~> expr) ^^ GDef 
+//    {
+//    case fname ~ pat ~ Some(vars) ~ body => GDef(fname, pat, vars, body)
+//    case fname ~ pat ~ None ~ body => GDef(fname, pat, List(), body)
+//  }
+  
+  def gDefVars = opt("," ~> repsep(variable, ",")) ^^ {
+    case Some(v) => v
+    case None => List[Var]()
+  }
+    
+  def ctorName = 
+    elem("Constructor name", x => x.isInstanceOf[lexical.Identifier] && x.chars.head.isUpper) ^^
+    {_.chars}
+  
+  def funcName = 
+    elem("Function name", x => x.isInstanceOf[lexical.Identifier] && x.chars.head.isLower) ^^
+    {_.chars}
+  
+  def expr = fCall | ctor | variable
+  
+  def pat = ctorName ~ ("(" ~> repsep(variable, ",") <~ ")") ^^ Pat
+    
+  def variable: Parser[Var] = ident ^^ Var
+  
+  def fCall: Parser[Expr] = funcName ~ ("(" ~> repsep(variable, ",") <~ ")") ^^ FCall
+  
+  def ctor: Parser[Expr] = ctorName ~ ("(" ~> repsep(expr, ",") <~ ")") ^^ Ctor
+  
+  def parseAll[T](p: Parser[T], in: String): ParseResult[T] = {
+    phrase(p)(new lexical.Scanner(in))
+  }
+  
+  def parseDefs(p: String) = parseAll(start, p) match {
+     case t if t.successful => t.get
+     case t => error(t.toString)
+     }
+}
---
+++ src/main/scala/sll/Syntax.scala
@@ -0,0 +1,13 @@
+package sll.syntax
+
+sealed abstract class Expr
+sealed abstract class Definition
+sealed abstract class Pattern
+
+case class Var(name: String) extends Expr
+case class Ctor(name: String, args: List[Expr]) extends Expr
+case class FCall(name: String, args: List[Expr]) extends Expr
+case class Pat(name: String, args: List[Var]) extends Pattern 
+case class FDef(name: String, params: List[Expr], body: Expr) extends Definition
+case class GDef(name: String, pat: Pattern, params: List[Expr], body: Expr) extends Definition
+case class Program(defs: List[Definition])
